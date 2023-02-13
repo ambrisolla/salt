4d994e9ad8a206f82d6afd193ad1ade6733fa8a7
ambrisolla <andremuzel@gmail.com>
2023-02-13 14:44:43 -0300

    Testing actions

--- .github/workflows/scripts/validate.sh
+++ .github/workflows/scripts/validate.sh
@@ -2,25 +2,40 @@
 
 
 TEST_STATES_DIR=$1
-TEST_SALT_ENV=$2
+TEST_PILLAR_DIR=$2
+TEST_SALT_ENV=$3
+
+
 LOG_FILE="/tmp/$(echo $0 | awk -F'/' '{print $NF}')-error.log"
 
+# States
 STATES=$( 
   find ${TEST_STATES_DIR} -name "*.sls" | \
   egrep -v "top.sls$|\/reactor\/|\/win/\repo-ng\/" | \
   sed "s/\/init.sls//g;s/.sls//g" | \
   sed "s@${TEST_STATES_DIR}\/@@g" | \
   sed "s/\//./g" )
-
 for state in ${STATES[@]}
 do
-  #echo -ne " - checking state ${state}... \r"  
-  test=$( salt-call state.sls_exists ${state} saltenv=${TEST_SALT_ENV} --out=json 2> ${LOG_FILE} | jq -r '.local' )
+  test=$( salt-call state.sls_exists ${state} saltenv=${TEST_SALT_ENV} --out=json 2>> ${LOG_FILE} | jq -r '.local' )
   case "${test}" in
     true)  message="PASSED" ;;
     false) message="FAILED" ;;
   esac
-  echo -ne " - checking state ${state}... ${message}\n"
+  echo -ne " - checking state ${state}... ${message}\n\n"
+  status_list+=(${test})
+done
+
+# Pillars
+PILLARS=$( find ${TEST_PILLAR_DIR} -name "*.sls" )
+for pillar in ${PILLARS[@]}
+do
+test=$( salt-call pillar.file_exists ${pillar} saltenv=${TEST_SALT_ENV} --out=json 2>> ${LOG_FILE} | jq -r '.local' )
+  case "${test}" in
+    true)  message="PASSED" ;;
+    false) message="FAILED" ;;
+  esac
+  echo -ne " - checking pillar file ${pillar}... ${message}\n"
   status_list+=(${test})
 done
 
