83bdabf8443c24ef00a3b4a7392e9333bf715c0c
ambrisolla <andremuzel@gmail.com>
2023-02-13 18:06:09 -0300

    Testing actions

--- .github/workflows/scripts/validate.sh
+++ .github/workflows/scripts/validate.sh
@@ -40,7 +40,7 @@ done
 if [[ "${status_list[*]}"  =~ "false" ]]
 then
   echo -ne "\n - Error: Failed to check Salt Pillars and Salt States !"
-  rm -rf ${PILLAR_DIR} ${STATES_DIR}
+  #rm -rf ${PILLAR_DIR} ${STATES_DIR}
   exit 1
 else
   echo -ne "\n - Success: Salt Pillar and Salt States check completed!"
