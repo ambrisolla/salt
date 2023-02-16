ffd3317e86f98a6c770dfbde5cef8a30a938c614
ambrisolla <andremuzel@gmail.com>
2023-02-16 13:49:25 -0300

    removing file-roots

--- .github/workflows/scripts/tests.py
+++ .github/workflows/scripts/tests.py
@@ -11,14 +11,14 @@ from   argparse   import ArgumentParser
 
 # set colors
 class message:
-  Black='\033[0;90m'       # Black
-  Red='\033[0;91m'         # Red
-  Green='\033[0;32m'       # Green
-  Yellow='\033[0;93m'      # Yellow
-  Blue='\033[0;94m'        # Blue
-  Purple='\033[0;95m'      # Purple
-  Cyan='\033[0;96m'        # Cyan
-  White='\033[0;97m'       # White
+  Black='\033[0;30m'        # Black
+  Red='\033[0;31m'          # Red
+  Green='\033[0;32m'        # Green
+  Yellow='\033[0;33m'       # Yellow
+  Blue='\033[0;34m'         # Blue
+  Purple='\033[0;35m'       # Purple
+  Cyan='\033[0;36m'         # Cyan
+  White='\033[0;37m'        # White
   Color_Off='\033[0m'      # Color Off
   success=f'{Green}SUCCESS{Color_Off}'
   warning=f'{Yellow}WARNING{Color_Off}'
